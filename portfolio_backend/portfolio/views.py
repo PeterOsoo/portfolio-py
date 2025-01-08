1e7065577891eb20c9b99e1bde593fc7b8178634
Peter Osoo <posoo617@gmail.com>
2025-01-08 21:46:39 +0300

    add views pages

--- portfolio_backend/portfolio/views.py
+++ portfolio_backend/portfolio/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from .models import About, Skill, Experience, Project, Contact
+from .serializers import AboutSerializer, SkillSerializer, ExperienceSerializer, ProjectSerializer, ContactSerializer
+
 # Create your views here.
+
+
+class AboutViewSet(ReadOnlyModelViewSet):
+    queryset = About.objects.all()
+    serializer_class = AboutSerializer
+
+
+class SkillViewSet(ReadOnlyModelViewSet):
+    queryset = Skill.objects.all()
+    serializer_class = SkillSerializer
+
+
+class ExperienceViewSet(ReadOnlyModelViewSet):
+    queryset = Experience.objects.all()
+    serializer_class = ExperienceSerializer
+
+
+class ProjectViewSet(ReadOnlyModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class ContactViewSet(ReadOnlyModelViewSet):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
