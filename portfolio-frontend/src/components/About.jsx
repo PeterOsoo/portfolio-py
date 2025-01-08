d9abea0499301f9d7ea64853c48054c2e75d0a30
Peter Osoo <posoo617@gmail.com>
2025-01-08 22:16:00 +0300

    add About Component

---
+++ portfolio-frontend/src/components/About.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react"
+import axios from "../axios"
+
+const About = () => {
+	const [aboutData, setAboutData] = useState(null)
+
+	useEffect(() => {
+		axios
+			.get("about/")
+			.then(response => {
+				setAboutData(response.data[0]) // assuming there's only one "About" entry
+			})
+			.catch(error => console.error(error))
+	}, [])
+
+	if (!aboutData) return <div>Loading...</div>
+
+	return (
+		<section>
+			<h2>About Me</h2>
+			<img src={aboutData.profile_image} alt={aboutData.name} />
+			<h3>{aboutData.name}</h3>
+			<p>{aboutData.bio}</p>
+		</section>
+	)
+}
+
+export default About
