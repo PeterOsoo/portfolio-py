d60a800f5cec8b05c32f904b60711eceed185f33
Peter Osoo <posoo617@gmail.com>
2025-01-08 22:40:55 +0300

    create Home Page

---
+++ portfolio-frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+	return (
+		<div>
+			<h2>Welcome to My Portfolio</h2>
+			<p>Explore the sections using the navigation links above.</p>
+		</div>
+	)
+}
+
+export default Home
