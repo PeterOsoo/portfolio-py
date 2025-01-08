0fd3dadd40ebe92c9e9e12d1f769b31b0c6febf4
Peter Osoo <posoo617@gmail.com>
2025-01-08 22:56:56 +0300

    add Navbar bootstrap

--- portfolio-frontend/src/App.jsx
+++ portfolio-frontend/src/App.jsx
@@ -1,58 +1,22 @@
-import { useEffect } from "react"
-
-import "./App.css"
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import NavBar from "./components/Navbar"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
 import About from "./components/About"
-import Skills from "./components/Skills"
-import Experience from "./components/Experience"
 import Projects from "./components/Projects"
 import Contact from "./components/Contact"
-import Home from "./pages/Home"
+
+import "./App.css"
 
 const App = () => {
-	useEffect(() => {
-		window.scrollTo(0, 0) // Scroll to top when route changes
-	}, [])
-
 	return (
 		<Router>
-			<div className="App">
-				<header className="App-header">
-					<h1>My Portfolio</h1>
-					<nav>
-						<ul>
-							<li>
-								<Link to="/">Home</Link>
-							</li>
-							<li>
-								<Link to="/about">About</Link>
-							</li>
-							<li>
-								<Link to="/skills">Skills</Link>
-							</li>
-							<li>
-								<Link to="/experience">Experience</Link>
-							</li>
-							<li>
-								<Link to="/projects">Projects</Link>
-							</li>
-							<li>
-								<Link to="/contact">Contact</Link>
-							</li>
-						</ul>
-					</nav>
-				</header>
-				<main>
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/about" element={<About />} />
-						<Route path="/skills" element={<Skills />} />
-						<Route path="/experience" element={<Experience />} />
-						<Route path="/projects" element={<Projects />} />
-						<Route path="/contact" element={<Contact />} />
-					</Routes>
-				</main>
-			</div>
+			<NavBar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/projects" element={<Projects />} />
+				<Route path="/contact" element={<Contact />} />
+			</Routes>
 		</Router>
 	)
 }
