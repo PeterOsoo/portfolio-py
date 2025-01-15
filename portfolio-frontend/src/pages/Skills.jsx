14fc7522fef093e701e149ed4948c77ec818cd02
Peter Osoo <posoo617@gmail.com>
2025-01-15 21:32:49 +0300

    style component

--- portfolio-frontend/src/pages/Skills.jsx
+++ portfolio-frontend/src/pages/Skills.jsx
@@ -1,14 +1,22 @@
 import { useState, useEffect } from "react"
 import axiosInstance from "../utils/axiosInstance"
+import Loading from "../components/Loading"
 
 const Skills = () => {
 	const [skills, setSkills] = useState([])
+	const [loading, setLoading] = useState(true) // State to handle loading
 
 	useEffect(() => {
 		axiosInstance
 			.get("skills/")
-			.then(response => setSkills(response.data))
-			.catch(error => console.error("Error fetching skills:", error))
+			.then(response => {
+				setSkills(response.data)
+				setLoading(false) // Set loading to false when data is fetched
+			})
+			.catch(error => {
+				console.error("Error fetching skills:", error)
+				setLoading(false) // Set loading to false even if there's an error
+			})
 	}, [])
 
 	return (
@@ -17,13 +25,18 @@ const Skills = () => {
 			<div className="row justify-content-center">
 				<div className="col-md-6">
 					<div className="card" style={{ width: "100%" }}>
-						<ul className="list-group list-group-flush">
-							{skills.map(skill => (
-								<li className="list-group-item" key={skill.id}>
-									{skill.name}
-								</li>
-							))}
-						</ul>
+						{/* Show loading indicator when data is being fetched */}
+						{loading ? (
+							<Loading />
+						) : (
+							<ul className="list-group list-group-flush">
+								{skills.map(skill => (
+									<li className="list-group-item" key={skill.id}>
+										{skill.name}
+									</li>
+								))}
+							</ul>
+						)}
 					</div>
 				</div>
 			</div>
