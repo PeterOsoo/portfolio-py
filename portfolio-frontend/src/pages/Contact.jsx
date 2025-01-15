df9f00181e7da2f61a011556d78da54c5e6f049e
Peter Osoo <posoo617@gmail.com>
2025-01-15 21:19:07 +0300

    style contact page

--- portfolio-frontend/src/pages/Contact.jsx
+++ portfolio-frontend/src/pages/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
 	useEffect(() => {
 		// Fetch the contact data from API
 		axiosInstance
-			.get("contact/") //
+			.get("contact/") // Adjust the endpoint as needed
 			.then(response => {
 				setContact(response.data[0])
 				console.log(response.data[0])
@@ -18,10 +18,14 @@ const Contact = () => {
 	}, [])
 
 	return (
-		<div>
+		<div className="d-flex justify-content-center align-items-center vh-100">
 			{contact ? (
-				<div>
-					<h1>Contact</h1>
+				<div className="jumbotron p-5 my-5 text-center">
+					<h1 className="display-4">Contact Me</h1>
+					<p className="lead">
+						Get in touch with me using the following details.
+					</p>
+					<hr className="my-4" />
 					<p>Email: {contact.email}</p>
 					<p>Phone: {contact.phone}</p>
 					<p>
@@ -30,8 +34,9 @@ const Contact = () => {
 							href={contact.linkedin}
 							target="_blank"
 							rel="noopener noreferrer"
+							className="btn btn-secondary"
 						>
-							{contact.linkedin}
+							Visit My LinkedIn
 						</a>
 					</p>
 				</div>
