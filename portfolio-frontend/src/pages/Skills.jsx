013716571a467ff8fed3f0cf9df57cd1f0bdd414
Peter Osoo <posoo617@gmail.com>
2025-01-15 20:13:13 +0300

    style list for skills page

--- portfolio-frontend/src/pages/Skills.jsx
+++ portfolio-frontend/src/pages/Skills.jsx
@@ -6,19 +6,27 @@ const Skills = () => {
 
 	useEffect(() => {
 		axiosInstance
-			.get("skills/") // No need to include the full URL
+			.get("skills/")
 			.then(response => setSkills(response.data))
 			.catch(error => console.error("Error fetching skills:", error))
 	}, [])
 
 	return (
-		<div>
-			<h1>Skills</h1>
-			<ul>
-				{skills.map(skill => (
-					<li key={skill.id}>{skill.name}</li>
-				))}
-			</ul>
+		<div className="container my-5">
+			<h1 className="text-center mb-4">Skills</h1>
+			<div className="row justify-content-center">
+				<div className="col-md-6">
+					<div className="card" style={{ width: "100%" }}>
+						<ul className="list-group list-group-flush">
+							{skills.map(skill => (
+								<li className="list-group-item" key={skill.id}>
+									{skill.name}
+								</li>
+							))}
+						</ul>
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
